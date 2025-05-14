72e8c7fdf6eeb80a80399f78fd0a0b0a0b855901
triantara22 <nandangtrirx@gmail.com>
2025-05-14 07:58:19 +0700

    menghapus route dan controller

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,9 +9,4 @@ $routes->setAutoRoute(true);
 $routes->get('/Home', 'Home::index');
 
 // Mobil routes
-$routes->get('/mobil', 'MobilController::index');
-$routes->get('/mobil/detail/(:num)', 'MobilController::detail/$1');
-$routes->post('/mobil/tambah', 'MobilController::tambah');
-$routes->get('/mobil/filter/(:any)', 'MobilController::filter/$1');
-$routes->put('/mobil/update/(:num)', 'MobilController::update/$1');
-$routes->post('/mobil/delete/(:num)', 'MobilController::delete/$1');
+
