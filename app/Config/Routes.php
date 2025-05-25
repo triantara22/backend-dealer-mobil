e337368e06e984f7bd032969d510b3913379985e
triantara22 <nandangtrirx@gmail.com>
2025-05-25 08:56:59 +0700

    Menambahkan feature/pelanggan

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,22 +5,28 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->setAutoRoute(true);
+$routes->setTranslateURIDashes(false);
+$routes->setAutoRoute(false);
 $routes->get('/Home', 'Home::index');
-
-
 
 $routes->post('/login', 'LoginController::login');
 $routes->options('/login', 'LoginController::login');
 
 $routes->group('', ['filter' => 'Auth:admin'], function ($routes) {
 
-$routes->get('/mobil', 'MobilController::index');
-$routes->get('/mobil/detail/(:num)', 'MobilController::detail/$1');
-$routes->post('/mobil/create', 'MobilController::create');
-$routes->get('/mobil/datafilter', 'MobilController::ambildatafilter');
-$routes->get('/mobil/filter/(:any)', 'MobilController::filter/$1');
-$routes->put('/mobil/update/(:num)', 'MobilController::update/$1');
-$routes->delete('/mobil/delete/(:num)', 'MobilController::delete/$1');
+    $routes->get('/mobil', 'MobilController::index');
+    $routes->get('/mobil/detail/(:num)', 'MobilController::detail/$1');
+    $routes->post('/mobil/create', 'MobilController::create');
+    $routes->get('/mobil/datafilter', 'MobilController::ambildatafilter');
+    $routes->get('/mobil/filter/(:any)', 'MobilController::filter/$1');
+    $routes->put('/mobil/update/(:num)', 'MobilController::update/$1');
+    $routes->delete('/mobil/delete/(:num)', 'MobilController::delete/$1');
+
+    $routes->get('/pelanggan', 'PelangganController::index');
+    $routes->get('/pelanggan/filter/(:any)', 'PelangganController::filter/$1');
+    $routes->get('/pelanggan/detail/(:segment)', 'PelangganController::detail/$1');
+    $routes->post('/pelanggan/create', 'PelangganController::create');
+    $routes->put('/pelanggan/update/(:segment)', 'PelangganController::update/$1');
+    $routes->delete('/pelanggan/delete/(:segment)', 'PelangganController::delete/$1');
 
 });
---
+++ app/Controllers/PelangganController.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\API\ResponseTrait;
+use App\Models\PelangganModel;
+use Exception;
+class PelangganController extends BaseController
+{
+    protected $pelangganmodel;
+    use ResponseTrait;
+
+    public function __construct()
+    {
+        $this->pelangganmodel = new PelangganModel();
+    }
+    public function index()
+    {
+        try {
+            $data = $this->pelangganmodel->getpelanggan();
+            if (empty($data)) {
+                return $this->response->setStatusCode(404)->setJSON([
+                    'status'  => false,
+                    'message' => 'Data Mobil Tidak Tersedia',
+                    'data'    => [],
+                ]);
+            }
+            return $this->response->setStatusCode(200)->setJSON([
+                'status'  => true,
+                'message' => 'Data Ditemukan',
+                'data'    => $data,
+            ]);
+        } catch (Exception $e) {
+            return $this->response->setStatusCode(500)->setJSON([
+                'Status'  => false,
+                'message' => "Internal server eror" . $e->getMessage(),
+                'data'    => [],
+            ]);
+        }
+    }
+
+    public function detail($id)
+    {
+        try {
+            $data = $this->pelangganmodel->find($id);
+            return $this->response->setStatusCode(200)->setJSON([
+                'status'  => true,
+                'message' => 'Data Ditemukan',
+                'data'    => $data,
+            ]);
+        } catch (Exception $e) {
+            return $this->response->setStatusCode(500)->setJSON([
+                'Status'  => false,
+                'message' => "Internal server eror" . $e->getMessage(),
+                'data'    => [],
+            ]);
+        }
+    }
+    public function create()
+    {
+        $id = $this->pelangganmodel->generateTransactionId();
+        $datapelanggan = [
+            'id'      => $id,
+            'nama'    => $this->request->getVar('nama'),
+            'alamat'  => $this->request->getVar('alamat'),
+            'telepon' => $this->request->getVar('telepon'),
+            'email'   => $this->request->getVar('email'),
+        ];
+
+        $this->pelangganmodel->insert($datapelanggan, true);
+        return $this->respondCreated([
+            'status'  => true,
+            'message' => 'Data Berhasil Ditambahkan',
+            'data'    => [
+                $datapelanggan,
+            ],
+        ]);
+    }
+
+    public function update($id)
+    {
+        $datapelanggan = [
+            'nama'    => $this->request->getVar('nama'),
+            'alamat'  => $this->request->getVar('alamat'),
+            'telepon' => $this->request->getVar('telepon'),
+            'email'   => $this->request->getVar('email'),
+        ];
+        $this->pelangganmodel->update($id, $datapelanggan);
+        return $this->respondUpdated([
+            'status'  => true,
+            'message' => 'Data Berhasil Diupdate',
+            'data'    => [
+                $datapelanggan,
+            ],
+        ]);
+    }
+
+    public function delete($id)
+    {
+        $this->pelangganmodel->delete($id);
+        return $this->respondDeleted([
+            'status'  => true,
+            'message' => 'Data Berhasil Dihapus',
+            'data'    => [],
+        ]);
+    }
+
+    public function filter($nama)
+    {
+        try {
+        $data = $this->pelangganmodel->filternama($nama);
+        if(empty($data)){
+            return $this->response->setStatusCode(404)->setJSON([
+                'status'  => false,
+                'message' => 'Data Mobil Tidak Tersedia',
+                'data'    => [],
+            ]);
+        }
+        return $this->response->setStatusCode(200)->setJSON([
+            'status'  => true,
+            'message' => 'Data Ditemukan',
+            'data'    => $data,
+        ]);
+        } catch (Exception $e) {
+            return $this->response->setStatusCode(500)->setJSON([
+                'Status'  => false,
+                'message' => "Internal server eror" . $e->getMessage(),
+                'data'    => [],
+            ]);
+        }
+    }
+
+}
